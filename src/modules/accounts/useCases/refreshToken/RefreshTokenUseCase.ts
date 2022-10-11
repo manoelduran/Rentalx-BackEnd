7b1dd2aa2f2c9855401fb8c7679f03a22498d266
Manoel Duran <manoel.duran@hotmail.com>
2022-10-11 17:54:57 -0300

    finished api

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -9,6 +9,11 @@ interface IPayload {
     sub: string;
     email: string;
 }
+
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -17,7 +22,7 @@ class RefreshTokenUseCase {
         @inject("DayjsDateProvider")
         private dayjsDateProvider: IDateProvider
     ) { }
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
         const user_id = sub;
         const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
@@ -38,7 +43,12 @@ class RefreshTokenUseCase {
             refresh_token,
             user_id,
         })
-        return refresh_token;
+
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        });
+        return {refresh_token, token: newToken};
     }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -14,18 +14,14 @@ interface IPayload {
 
 const ensureAuthenticated = async (request: Request, response: Response, next: NextFunction) => {
     const authHeader = request.headers.authorization;
-    const usersTokensRepository = new UsersTokensRepository();
+
     if (!authHeader) {
         throw new AppError("You don't have a token!", 401)
     };
     const [, token] = authHeader.split(" ");
     try {
-        const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload;
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
         console.log(user_id);
-        const selectedUser = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-        if (!selectedUser) {
-            throw new AppError("User does not exists!", 401);
-        };
         request.user = {
             id: user_id,
         };
